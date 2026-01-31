7d222f7d8aebdc789866e16bfcb5fe44211d8b6b
SakthiPrabhurajan <138732741+SakritUser123@users.noreply.github.com>
2026-01-31 13:40:52 -0500

    Update app.py

--- app.py
+++ app.py
@@ -144,8 +144,6 @@ with tab4:
     st.title("Find Mental Health Institutions Near Me")
     st.info("This link will take to to nearby Mental Hospitals in your area: " )
     qquery = "Mental Hospitals"
-    st.write(
-        f'<iframe src="https://www.google.com/maps/search/{qquery}"></iframe>',
-        unsafe_allow_html=True,
-    )
+    search_urll = f"https://www.google.com/maps/search{qquery)"
+    st.markdown(f'<a href="{search_urll}" target="_blank">Click to search: {qquery}</a>', unsafe_allow_html=True)
 
